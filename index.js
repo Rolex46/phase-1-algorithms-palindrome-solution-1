656dc7db51a1395f7ce4b06830d112d858bbbabd
Rolex Omondi <rolexomondi46@gmail.com>
2023-03-30 15:13:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  //write a function. inside it pass an argument for a string.in the body of the function use 
+  //if statements to check if the word reads the same from whichever direction it is looked at. if so, return true, otherwise return false
 
-/* 
-  Add your pseudocode here
-*/
+if (word === word.split("").reverse().join("")) {
+  return true
+  }
+  else
+      return false
+  };
+  isPalindrome("madam");
+
 
 /*
   Add written explanation of your solution here
+  write a funtion called isPalindrome.
+  pass one argument
+  pass an if statement with strict comparison operator
+  if word is palindrome, return true
+  otherwise return false
 */
 
 // You can run `node index.js` to view these console logs
